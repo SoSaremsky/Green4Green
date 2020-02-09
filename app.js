048bf547ceb60a2b0c36f7dc96751139ad621d2c
Jack Albertson <jacka73@verizon.net>
2020-02-09 03:46:50 -0500

    Welcome to Express

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
-
+var multer  = require('multer')
+var upload = multer()
+let path = require("path");
+let duck;
 
 const app = express();
 app.use(express.json());
@@ -7,7 +10,19 @@ app.get((res, req)=>{
   const goat = JSON.Stringify(req.body);
   console.log(goat);
 })
-app.use(express.static("./public"));
+app.use(express.json());
+app.post('/submission', upload.none(), function (req, res, next) {
+  duck = req.body;
+  res.sendFile(__dirname + "/Bing Hack 2020/results.html");
+})
+
+app.get("/budData", (req, res)=>{
+  res.send(duck);
+  console.log(req);
+})
+
+app.use(express.static("./Bing\ Hack\ 2020"));
+
 
 
 app.listen(5000, ()=>console.log("connected"));
