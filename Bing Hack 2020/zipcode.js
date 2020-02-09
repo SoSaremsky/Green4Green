9a4ec3584d8fd019311ff2464d8f5d0cbdb13bce
Sophie Saremsky <ssarems1@binghamton.edu>
2020-02-09 11:05:42 -0500

    fixes

--- Bing Hack 2020/zipcode.js
+++ Bing Hack 2020/zipcode.js
@@ -1,34 +1,88 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var reqhttp = new XMLHttpRequest();
+import { require } from "require";
 
-var url = "https://pcmiler.alk.com/apis/rest/v1.0/service.svc/locations?postcode=";
-//var zip = document.getElementById("zip");
-var zip = 10603
-url = url + zip;
+//temp lat < 40 = hot
+//temp lat >= 40 = cold
+//sunshine 29 < sun <=50   low
+//sunshine 50 < sun <= 70  med
+//sunshine 70 < sun <= 90  hi
+
+function coordsByZipcode(){
+
+	var location = {
+		sunlevel: 0,
+		commType: "",
+		temp: ""
+	};
+
+  var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+  var reqhttp = new XMLHttpRequest();
+
+  var zip = document.getElementById("zip").value
+  var url = "https://pcmiler.alk.com/apis/rest/v1.0/service.svc/locations?postcode=";
+  url = url + zip;
+
+  reqhttp.responseType = "json";
+  reqhttp.open("GET", url, true);
+  reqhttp.setRequestHeader("Content-type", "application/json");
+	reqhttp.setRequestHeader("Authorization", "910771335EB6744C9F9449F304EC3FF5");
+  reqhttp.send();
 
 
-reqhttp.responseType = "json";
-reqhttp.open("GET", url, true);
-reqhttp.setRequestHeader("Content-type", "application/json");
-reqhttp.setRequestHeader("Authorization", "910771335EB6744C9F9449F304EC3FF5");
+  var coords = [];
 
-reqhttp.send();
-
-reqhttp.onreadystatechange = processRequest;
-
-function processRequest(e) {
-  if (reqhttp.readyState == 4 && reqhttp.status == 200) {
-      var response = JSON.parse(reqhttp.responseText);
-      console.log(response[0].Coords.Lat);
-      console.log(response[0].Coords.Lon);
+  reqhttp.onreadystatechange = processRequest;
+  function processRequest(e) {
+    if (reqhttp.readyState == 4 && reqhttp.status == 200) {
+        var response = JSON.parse(reqhttp.responseText);
+        coords[0] = response[0].Coords.Lat;
+        coords[1] = response[0].Coords.Lon;
+		var city = distanceCities(coords[0], coords[1]);
+		var fs = require('fs');
+		var textByLine = fs.readFileSync('data.txt').toString().split("\n");
+		var parts = textByLine[city].toString().split(" #");
+		var sunshine = parseFloat(parts[3].substring(10));
+		var temp = 1; //cold
+		if (sunshine >= 30 && sunshine <= 50)
+			sunshine = 1;
+		else if (sunshine > 50 && sunshine <= 70)
+			sunshine = 2;
+		else
+			sunshine = 3;
+		if (coords[0] > 45)
+			temp = 2;
+		location.sunlevel = sunshine;
+		if(temp === 1){
+			location.temp = "cold";
+		}else{
+			location.temp = "hot";
+		}
+		location.commType = document.getElementById("community").value;
+		localStorage.setItem("loc", location);i
+    }
   }
 }
 
+function distanceCities(lat1,lon1){
+	var fs = require('fs');
+	var textByLine = fs.readFileSync('data.txt').toString().split("\n");
+	var init = textByLine[0].toString().split(" #");
+	var smol = distance(lat1, lon1, parseFloat(init[1].substring(5)), parseFloat(init[2].substring(6)));
+	var index = 0;
+	for (var i = 1; i < 155; i++){
+		parts = textByLine[i].toString().split(" #");
+		var dist = distance(lat1, lon1, parseFloat(parts[1].substring(5)), parseFloat(parts[2].substring(6)));
+		if (dist < smol){
+			smol = dist;
+			index = i;
+		}
+	}
+	return index;
+}
 
-function hehe(city, state){
+function coordsFromCity(city, state){
     var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
     var xml = new XMLHttpRequest();
-    var url = "https://pcmiler.alk.com/apis/rest/v1.0/Service.svc/locations?city="
+    var url = "https://cors-anywhere.herokuapp.com/https://pcmiler.alk.com/apis/rest/v1.0/Service.svc/locations?city="
     url = url + city;
     var url2 = "&state="
     url2 = url2 + state;
@@ -46,20 +100,30 @@ function hehe(city, state){
     function processRequest(e) {
       if (xml.readyState == 4 && xml.status == 200) {
           var response = JSON.parse(xml.responseText);
-          console.log(city);
           console.log(response[0].Coords.Lat);
           console.log(response[0].Coords.Lon);
       }
     }
 }
 
-function helppp(){
-  var fs = require('fs');
-  var textByLine = fs.readFileSync('sunshine.txt').toString().split("\n");
-  for (var i = 0; i < 157; i++){
-    var split = textByLine[i].toString().replace(",","\t").split("\t");
-    console.log(split[2].trim().substring(0,2));
-    //hehe(split[0].trim(),split[1].trim());
-  }
+function distance(lat1, lon1, lat2, lon2) {
+	if ((lat1 == lat2) && (lon1 == lon2)) {
+		return 0;
+	}
+	else {
+		var radlat1 = Math.PI * lat1/180;
+		var radlat2 = Math.PI * lat2/180;
+		var theta = lon1-lon2;
+		var radtheta = Math.PI * theta/180;
+		var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+		if (dist > 1) {
+			dist = 1;
+		}
+		dist = Math.acos(dist);
+		dist = dist * 180/Math.PI;
+		dist = dist * 60 * 1.1515;
+		return dist;
+	}
 }
-helppp()
+
+coordsByZipcode("90009");
